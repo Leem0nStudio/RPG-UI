b1f64a89be9ce11b2d1c27db0f956f2f415890a8
Leem0nStudio <leemon.studio@gmail.com>
2026-04-21 23:01:43 +0000

    fix reset SQL to drop tables first then functions

--- supabase/001_reset.sql
+++ supabase/001_reset.sql
@@ -3,18 +3,18 @@
 
 drop trigger if exists on_auth_user_created on auth.users;
 drop function if exists public.handle_new_player;
+
+drop table if exists public.player_quest_progress;
+drop table if exists public.player_items;
+drop table if exists public.player_units;
+drop table if exists public.player_currencies;
+drop table if exists public.player_profiles;
+drop table if exists public.enemy_definitions;
+drop table if exists public.summon_banners;
+drop table if exists public.quest_definitions;
+drop table if exists public.item_definitions;
+drop table if exists public.unit_definitions;
+drop table if exists public.job_definitions;
+
 drop function if exists public.update_quest_progress_timestamp;
-
-drop table if exists public.player_quest_progress cascade;
-drop table if exists public.player_items cascade;
-drop table if exists public.player_units cascade;
-drop table if exists public.player_currencies cascade;
-drop table if exists public.player_profiles cascade;
-drop table if exists public.enemy_definitions cascade;
-drop table if exists public.summon_banners cascade;
-drop table if exists public.quest_definitions cascade;
-drop table if exists public.item_definitions cascade;
-drop table if exists public.unit_definitions cascade;
-drop table if exists public.job_definitions cascade;
-
 drop extension if exists "pgcrypto";
